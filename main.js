0d109cde3df679ad1cf7fab58d3ea979dfcc410e
janitastic <76501557+janitastic@users.noreply.github.com>
2021-09-08 23:47:56 -0600

    Create user selection variables

---
+++ main.js
@@ -0,0 +1,8 @@
+// Variables
+
+var affirmationSelection = getElementById('affirmation');
+var mantraSelection = getElementById('mantra');
+var messageBtn = querySelector('.message-btn');
+
+
+// Event Listeners
