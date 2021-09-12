52457fc3b15790e98f4556f64d6dc7dceab526be
janitastic <76501557+janitastic@users.noreply.github.com>
2021-09-11 21:16:21 -0600

    Modify error message to display above button

--- main.js
+++ main.js
@@ -7,10 +7,13 @@ var messageBox = document.querySelector('.message');
 var messageDisplay = document.getElementById('message-display');
 var buttonDisplay = document.getElementById('button-display');
 var clearBtn = document.getElementById('clear');
+var errorMessage = document.getElementById('error');
 
 // Event Listeners
 messageBtn.addEventListener("click", showMessage);
 clearBtn.addEventListener("click", clearMessage);
+affirmationRadio.addEventListener("click", hideErrorMessage);
+mantraRadio.addEventListener("click", hideErrorMessage);
 
 // Functions
 function getRandomIndex(array) {
@@ -20,6 +23,7 @@ function getRandomIndex(array) {
 function showMessage() {
   if (affirmationRadio.checked) {
     displayAffirmation();
+    // hideErrorMessage();
     affirmationRadio.checked = false;
   } else if (mantraRadio.checked) {
     displayMantra();
@@ -31,21 +35,43 @@ function showMessage() {
 
 function displayAffirmation() {
   messageDisplay.innerText = affirmations[getRandomIndex(affirmations)];
-  buttonDisplay.classList.remove("hidden");
+  // buttonDisplay.classList.remove("hidden");
+  show(buttonDisplay);
 }
 
 function displayMantra() {
   messageDisplay.innerText = mantras[getRandomIndex(mantras)];
-  buttonDisplay.classList.remove("hidden");
+  // buttonDisplay.classList.remove("hidden");
+  show(buttonDisplay);
 }
 
 function showErrorMessage() {
-  if (!affirmationRadio.checked & !mantraRadio.checked) {
-    messageBox.innerHTML = `<p class="error">Oops! Please make a selection.</p>`;
+  if (!affirmationRadio.checked && !mantraRadio.checked) {
+    show(errorMessage);
   }
 }
 
 function clearMessage() {
   messageDisplay.innerHTML = `<img src="assets/meditate.svg" alt="Meditate bell">`;
-  buttonDisplay.classList.add("hidden");
+  // buttonDisplay.classList.add("hidden");
+  hide(buttonDisplay);
+}
+
+//Helper Functions
+function hide(element) {
+  element.classList.add('hidden')
+}
+
+function show(element) {
+  element.classList.remove('hidden')
+}
+
+function fade(element) {
+  element.classList.toggle('hidden')
+}
+
+function hideErrorMessage() {
+  if (affirmationRadio.checked || mantraRadio.checked) {
+    hide(errorMessage);
+  }
 }
